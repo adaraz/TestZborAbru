851ddf0036fb5e088a885258c5640bfbeaa2e7de
Radek Mazar <radek.mazar@gmail.com>
2018-02-22 11:28:23 +0100

    Init

---
+++ ZborAbru/ZborAbru/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OLE = AbraOLELib;
+
+namespace ZborAbru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new OLE.Application();
+            var ids = string.Join(",", Enumerable.Range(1, 435).Select(x => x.ToString().PadLeft(10, '0')));
+            var sql = app.CreateSQLStatement();
+            sql.SQL = $"select value from string_split(:ids, ',')";
+            sql.BindParam("ids", ids);
+            sql.Execute();
+            Console.Write(sql.RowsAffected);
+            Console.ReadKey();
+        }
+    }
+}
